937a80cdb6f713828a2ddf6ab010e38dd5d876de
pzecevic <pzecevic@6d84d9ae-2a59-4de9-af64-c5327561e532>
2019-10-19 14:14:22 +0000

    Made delta element and list structures. Declared and defined methods for delta list. Method for adding element needs to implement delta logic.

--- delta_list/delta_list.c
+++ delta_list/delta_list.c
@@ -4,5 +4,29 @@
  *  Created on: Oct 16, 2019
  *      Author: root
  */
+#include <stdlib.h>
 #include "delta_list.h"
 
+static void destroyDeltaElement(void* element)
+{
+  DeltaElement_t* deltaElement = (DeltaElement_t*)element;
+  free(deltaElement->object);
+  free(deltaElement);
+}
+
+void DeltaList_Initialize(DeltaList_t* deltaList)
+{
+  SingleLinkedList_Initialize(&(deltaList->rawList), &destroyDeltaElement);
+}
+
+void DeltaList_Add(DeltaList_t* deltaList, DeltaElement_t* newDeltaElement)
+{
+  ListElement_t** currentRawElement = &(deltaList->rawList.head);
+  /*TODO: Process delta list to find correct position for new element.*/
+  SingleLinkedList_Insert(currentRawElement, newDeltaElement);
+}
+
+void DeltaList_Destroy(DeltaList_t* deltaList)
+{
+  SingleLinkedList_Destroy(&(deltaList->rawList));
+}
